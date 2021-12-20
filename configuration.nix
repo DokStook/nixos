b6c80d00f3673958123e39bf0ec7011952d50b51
DokStook <31543930+DokStook@users.noreply.github.com>
2021-12-20 17:41:38 +0000

    Update after nix-up
    Update after nix-up

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,30 @@
+################################################################################
+# █▄░█ █ ▀▄▀ █▀█ █▀ ▄▄ █░█ █▀█
+# █░▀█ █ █░█ █▄█ ▄█ ░░ █▄█ █▀▀
+#
+# 🚀 This NixOS installation brought to you by nixos-up! 🚀
+# Please consider supporting the project (https://github.com/samuela/nixos-up)
+# and the NixOS Foundation (https://opencollective.com/nixos)!
+################################################################################
+
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, ...}:
 
+let
+  home-manager = fetchTarball "https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz";
+in
 {
-  imports =
-    [ # Include the results of the hardware scan.
+  # Your home-manager configuration! Check out https://rycee.gitlab.io/home-manager/ for all possible options.
+  home-manager.users.bananymous = { pkgs, ... }: {
+    home.packages = with pkgs; [ hello ];
+    programs.starship.enable = true;
+  };
+
+  imports = [ "${home-manager}/nixos" 
+ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
@@ -16,7 +34,7 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
+  
   # Set your time zone.
   time.timeZone = "Europe/Sofia";
 
@@ -32,21 +50,20 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   # Enable the Plasma 5 Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
   
-
   # Configure keymap in X11
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
@@ -61,25 +78,27 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.mutableUsers = false;
   users.users.bananymous = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" ];
+    passwordFile = "/etc/passwordFile-bananymous";
   };
 
+  # Disable password-based login for root.
+  users.users.root.hashedPassword = "!";
+
   # List packages installed in system profile. To search, run:
-  #  nix search wget
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
     # term
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    xkill
     alacritty
     fish
     # gui
     firefox
-    steam
     betterdiscord-installer
     qbittorrent
     xfce.thunar
@@ -93,18 +112,21 @@
 
   ];
 
+  # propr
+  nixpkgs.config.allowUnfree = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+   programs.mtr.enable = true;
+   programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+   };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -118,13 +140,13 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
+  system.stateVersion = "22.11"; # Did you read the comment?
 
+  # Configure swap file. Sizes are in megabytes. Default swap is
+  # max(1GB, sqrt(RAM)) = 1024. If you want to use hibernation with
+  # this device, then it's recommended that you use
+  # RAM + max(1GB, sqrt(RAM)) = 8802.000.
+  swapDevices = [ { device = "/swapfile"; size = 1024; } ];
 }
 
-   # GRUB 2 bootloader
-   boot.loader.grub.enable = true;
-   boot.loader.grub.version = 2;
-   boot.loader.grub.device = "/dev/nvme0n1p";
-
-
+pre { font-family: "Liberation Mono", monospace; font-size: 10pt; background: transparent }p { line-height: 115%; margin-bottom: 0.1in; background: transparent }
